00cf83ee2776108c719c4a6f1f9af065d3ec830a
AlanFor301 <allendiabolo@gmail.com>
2015-09-27 14:58:54 -0600

    launch page done

--- ReflectionTester/app/src/main/java/com/example/qyu4/reflectiontester/LaunchPage.java
+++ ReflectionTester/app/src/main/java/com/example/qyu4/reflectiontester/LaunchPage.java
@@ -1,37 +1,23 @@
 package com.example.qyu4.reflectiontester;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Handler;
 
-public class LaunchPage extends ActionBarActivity {
-
+public class LaunchPage extends Activity {
+    private Handler handler =  new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.launch_page);
+        handler.postDelayed(new Runnable() {
+            public void run() {
+                Intent openNewActivity = new Intent(getApplicationContext(), NumberChooseActivity.class);
+                startActivity(openNewActivity);
+            }
+        }, 2000);
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_launch_page, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ ReflectionTester/app/src/main/java/com/example/qyu4/reflectiontester/NumberChooseActivity.java
@@ -0,0 +1,16 @@
+package com.example.qyu4.reflectiontester;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class NumberChooseActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.number_choose);
+    }
+
+}
